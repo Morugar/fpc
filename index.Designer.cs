061b5986bba884b5231d5801e191bc0dffe51092
Morugar <morugar.shestak@gmail.com>
2022-07-22 16:04:34 +0300

    Init python version of FPC

---
+++ index.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace FPS_Python
+{
+    partial class index
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.projectName = new System.Windows.Forms.TextBox();
+            this.github_repo = new System.Windows.Forms.TextBox();
+            this.project_dir = new System.Windows.Forms.TextBox();
+            this.directoryDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.project_dir_button = new System.Windows.Forms.Button();
+            this.python_int = new System.Windows.Forms.TextBox();
+            this.python_int_button = new System.Windows.Forms.Button();
+            this.virtualenv_name = new System.Windows.Forms.TextBox();
+            this.virtualenv_checkbox = new System.Windows.Forms.CheckBox();
+            this.package_list = new System.Windows.Forms.ListBox();
+            this.packages_label = new System.Windows.Forms.Label();
+            this.add_package_button = new System.Windows.Forms.Button();
+            this.package_name = new System.Windows.Forms.TextBox();
+            this.delete_package_button = new System.Windows.Forms.Button();
+            this.requirements_checkbox = new System.Windows.Forms.CheckBox();
+            this.create_project = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // projectName
+            // 
+            this.projectName.Location = new System.Drawing.Point(35, 12);
+            this.projectName.MaxLength = 32;
+            this.projectName.Name = "projectName";
+            this.projectName.Size = new System.Drawing.Size(156, 20);
+            this.projectName.TabIndex = 0;
+            this.projectName.Text = "Enter your project name here";
+            // 
+            // github_repo
+            // 
+            this.github_repo.Location = new System.Drawing.Point(35, 51);
+            this.github_repo.MaxLength = 32;
+            this.github_repo.Name = "github_repo";
+            this.github_repo.Size = new System.Drawing.Size(156, 20);
+            this.github_repo.TabIndex = 1;
+            this.github_repo.Text = "Enter your github repo here";
+            // 
+            // project_dir
+            // 
+            this.project_dir.Location = new System.Drawing.Point(35, 88);
+            this.project_dir.Name = "project_dir";
+            this.project_dir.Size = new System.Drawing.Size(113, 20);
+            this.project_dir.TabIndex = 2;
+            this.project_dir.Text = "Etner project directory";
+            // 
+            // directoryDialog
+            // 
+            this.directoryDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.directoryDialog.SelectedPath = "C:\\Users";
+            // 
+            // project_dir_button
+            // 
+            this.project_dir_button.Location = new System.Drawing.Point(154, 88);
+            this.project_dir_button.Name = "project_dir_button";
+            this.project_dir_button.Size = new System.Drawing.Size(37, 20);
+            this.project_dir_button.TabIndex = 3;
+            this.project_dir_button.Text = "...";
+            this.project_dir_button.UseVisualStyleBackColor = true;
+            this.project_dir_button.Click += new System.EventHandler(this.project_dir_button_Click);
+            // 
+            // python_int
+            // 
+            this.python_int.Location = new System.Drawing.Point(35, 123);
+            this.python_int.Name = "python_int";
+            this.python_int.Size = new System.Drawing.Size(113, 20);
+            this.python_int.TabIndex = 4;
+            this.python_int.Text = "Etner python directory";
+            // 
+            // python_int_button
+            // 
+            this.python_int_button.Location = new System.Drawing.Point(154, 123);
+            this.python_int_button.Name = "python_int_button";
+            this.python_int_button.Size = new System.Drawing.Size(37, 20);
+            this.python_int_button.TabIndex = 5;
+            this.python_int_button.Text = "...";
+            this.python_int_button.UseVisualStyleBackColor = true;
+            this.python_int_button.Click += new System.EventHandler(this.python_int_button_Click);
+            // 
+            // virtualenv_name
+            // 
+            this.virtualenv_name.Location = new System.Drawing.Point(238, 12);
+            this.virtualenv_name.MaxLength = 32;
+            this.virtualenv_name.Name = "virtualenv_name";
+            this.virtualenv_name.Size = new System.Drawing.Size(156, 20);
+            this.virtualenv_name.TabIndex = 6;
+            this.virtualenv_name.Text = "Enter your virtualenv name here";
+            // 
+            // virtualenv_checkbox
+            // 
+            this.virtualenv_checkbox.AutoSize = true;
+            this.virtualenv_checkbox.Checked = true;
+            this.virtualenv_checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.virtualenv_checkbox.Location = new System.Drawing.Point(35, 158);
+            this.virtualenv_checkbox.Name = "virtualenv_checkbox";
+            this.virtualenv_checkbox.Size = new System.Drawing.Size(106, 17);
+            this.virtualenv_checkbox.TabIndex = 7;
+            this.virtualenv_checkbox.Text = "Create virtualenv";
+            this.virtualenv_checkbox.UseVisualStyleBackColor = true;
+            this.virtualenv_checkbox.CheckedChanged += new System.EventHandler(this.virtualenv_checkbox_CheckedChanged);
+            // 
+            // package_list
+            // 
+            this.package_list.FormattingEnabled = true;
+            this.package_list.Location = new System.Drawing.Point(238, 67);
+            this.package_list.Name = "package_list";
+            this.package_list.Size = new System.Drawing.Size(156, 95);
+            this.package_list.TabIndex = 8;
+            // 
+            // packages_label
+            // 
+            this.packages_label.AutoSize = true;
+            this.packages_label.Location = new System.Drawing.Point(235, 51);
+            this.packages_label.Name = "packages_label";
+            this.packages_label.Size = new System.Drawing.Size(58, 13);
+            this.packages_label.TabIndex = 9;
+            this.packages_label.Text = "Packages:";
+            // 
+            // add_package_button
+            // 
+            this.add_package_button.Location = new System.Drawing.Point(238, 193);
+            this.add_package_button.Name = "add_package_button";
+            this.add_package_button.Size = new System.Drawing.Size(75, 23);
+            this.add_package_button.TabIndex = 10;
+            this.add_package_button.Text = "Add";
+            this.add_package_button.UseVisualStyleBackColor = true;
+            this.add_package_button.Click += new System.EventHandler(this.add_package_button_Click);
+            // 
+            // package_name
+            // 
+            this.package_name.Location = new System.Drawing.Point(238, 165);
+            this.package_name.Name = "package_name";
+            this.package_name.Size = new System.Drawing.Size(156, 20);
+            this.package_name.TabIndex = 11;
+            this.package_name.Text = "Package name here";
+            // 
+            // delete_package_button
+            // 
+            this.delete_package_button.Location = new System.Drawing.Point(319, 193);
+            this.delete_package_button.Name = "delete_package_button";
+            this.delete_package_button.Size = new System.Drawing.Size(75, 23);
+            this.delete_package_button.TabIndex = 12;
+            this.delete_package_button.Text = "Delete";
+            this.delete_package_button.UseVisualStyleBackColor = true;
+            this.delete_package_button.Click += new System.EventHandler(this.delete_package_button_Click);
+            // 
+            // requirements_checkbox
+            // 
+            this.requirements_checkbox.AutoSize = true;
+            this.requirements_checkbox.Location = new System.Drawing.Point(238, 222);
+            this.requirements_checkbox.Name = "requirements_checkbox";
+            this.requirements_checkbox.Size = new System.Drawing.Size(134, 17);
+            this.requirements_checkbox.TabIndex = 13;
+            this.requirements_checkbox.Text = "Create requirements.txt";
+            this.requirements_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // create_project
+            // 
+            this.create_project.Location = new System.Drawing.Point(52, 195);
+            this.create_project.Name = "create_project";
+            this.create_project.Size = new System.Drawing.Size(96, 21);
+            this.create_project.TabIndex = 14;
+            this.create_project.Text = "Create";
+            this.create_project.UseVisualStyleBackColor = true;
+            this.create_project.Click += new System.EventHandler(this.create_project_Click);
+            // 
+            // index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 243);
+            this.Controls.Add(this.create_project);
+            this.Controls.Add(this.requirements_checkbox);
+            this.Controls.Add(this.delete_package_button);
+            this.Controls.Add(this.package_name);
+            this.Controls.Add(this.add_package_button);
+            this.Controls.Add(this.packages_label);
+            this.Controls.Add(this.package_list);
+            this.Controls.Add(this.virtualenv_checkbox);
+            this.Controls.Add(this.virtualenv_name);
+            this.Controls.Add(this.python_int_button);
+            this.Controls.Add(this.python_int);
+            this.Controls.Add(this.project_dir_button);
+            this.Controls.Add(this.project_dir);
+            this.Controls.Add(this.github_repo);
+            this.Controls.Add(this.projectName);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "index";
+            this.Text = "FPC Python";
+            this.Load += new System.EventHandler(this.index_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox projectName;
+        private System.Windows.Forms.TextBox github_repo;
+        private System.Windows.Forms.TextBox project_dir;
+        private System.Windows.Forms.FolderBrowserDialog directoryDialog;
+        private System.Windows.Forms.Button project_dir_button;
+        private System.Windows.Forms.TextBox python_int;
+        private System.Windows.Forms.Button python_int_button;
+        private System.Windows.Forms.TextBox virtualenv_name;
+        private System.Windows.Forms.CheckBox virtualenv_checkbox;
+        private System.Windows.Forms.ListBox package_list;
+        private System.Windows.Forms.Label packages_label;
+        private System.Windows.Forms.Button add_package_button;
+        private System.Windows.Forms.TextBox package_name;
+        private System.Windows.Forms.Button delete_package_button;
+        private System.Windows.Forms.CheckBox requirements_checkbox;
+        private System.Windows.Forms.Button create_project;
+    }
+}
+
---
+++ index.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace FPS_Python
+{
+    public partial class index : Form
+    {
+        public index()
+        {
+            InitializeComponent();
+        }
+
+        private void project_dir_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = directoryDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                project_dir.Text = directoryDialog.SelectedPath.ToString();
+            }
+        }
+
+        private void python_int_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = directoryDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                python_int.Text = directoryDialog.SelectedPath.ToString();
+            }
+        }
+
+        private void index_Load(object sender, EventArgs e)
+        {
+            string userName = Environment.UserName;
+            string userName2 = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+            var result = new bool[6];
+
+            var directories = new string[6] {"C:\\Program Files\\Python", "C:\\Python", "C:\\Users\\Python", "C:\\Users\\Default\\Python", "C:\\Users\\" + userName + "\\Python", "C:\\Users\\" + userName2 + "\\Python"};
+
+            result[0] = Directory.Exists("C:\\Program Files\\Python");
+            result[1] = Directory.Exists("C:\\Python");
+            result[2] = Directory.Exists("C:\\Users\\Python");
+            result[3] = Directory.Exists("C:\\Users\\Default\\Python");
+            result[4] = Directory.Exists("C:\\Users\\" + userName + "\\Python");
+            result[5] = Directory.Exists("C:\\Users\\" + userName2 + "\\Python");
+
+            foreach (var item in result.Select((item, index) => (item, index)))
+            {
+                if (item.item == true)
+                {
+                    python_int.Text = directories[item.index];
+                }
+            }
+        }
+
+        private void virtualenv_checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            int width1 = 445;
+            int height1 = 282;
+            int width2 = 237;
+            int height2 = 251;
+
+            if (virtualenv_checkbox.Checked == true)
+            {
+                this.MaximumSize = new Size(width1, height1);
+                this.MinimumSize = new Size(width1, height1);
+                this.Size = new Size(width1, height1);
+
+            }
+            else if(virtualenv_checkbox.Checked == false)
+            {
+                this.MaximumSize = new Size(width2, height2);
+                this.MinimumSize = new Size(width2, height2);
+                this.Size = new Size(width2, height2);
+
+            }
+        }
+
+        private void add_package_button_Click(object sender, EventArgs e)
+        {
+            string PackageName = package_name.Text;
+            package_list.Items.Add(PackageName);
+        }
+
+        private void delete_package_button_Click(object sender, EventArgs e)
+        {
+            string element = package_list.Text;
+            package_list.Items.Remove(element);
+        }
+
+        private void create_project_Click(object sender, EventArgs e)
+        {
+            string projectLabel = projectName.Text;
+            string projectDir = project_dir.Text;
+            string repo = github_repo.Text;
+            string pythonDir = python_int.Text;
+
+            string virtualenvLabel = virtualenv_name.Text;
+            string[] packages = new string[package_list.Items.Count];
+            bool requirements = requirements_checkbox.Checked;
+
+            for(int i = 0; i < package_list.Items.Count; i++)
+            {
+                packages.Append(package_list.Items[i].ToString());
+            }
+
+            try
+            {
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                // startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C mkdir " + projectDir + "\\" + projectLabel;
+                string projectPath = projectDir + "\\" + projectLabel;
+                string python = pythonDir + "\\python.exe";
+                cmd.StartInfo = startInfo;
+                cmd.Start();
+                Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+                if (virtualenvLabel != null || virtualenvLabel != "Enter your virtualenv name here")
+                {
+                    startInfo.Arguments = "/C cd " + projectPath + " && " + python + " -m venv " + virtualenvLabel;
+                    cmd.StartInfo = startInfo;
+                    cmd.Start();
+                    Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+                    startInfo.Arguments = "/C cd " + projectPath + " && " + "venv\\Scripts\\activate.bat";
+                    cmd.StartInfo = startInfo;
+                    cmd.Start();
+                    Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+                    for (int i = 0; i <= packages.Length; i++)
+                    {
+                        startInfo.Arguments = "/C cd " + projectPath + " && " + "venv\\Scripts\\activate.bat && " + pythonDir + "-m pip install " + packages[i];
+                        cmd.StartInfo = startInfo;
+                        cmd.Start();
+                        Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= packages.Length; i++)
+                    {
+                        startInfo.Arguments = "/C " + pythonDir + "-m pip install " + packages[i];
+                        cmd.StartInfo = startInfo;
+                        cmd.Start();
+                        Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
